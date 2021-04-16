7b889c53564694b06623c75e505524de95e24f0e
rrobles <rrobles@ext.cafpower.com>
2021-04-16 09:40:42 +0200

    Añadidas constantes para país e índice. Se añaden resultados latentes para la semana actual y cartera provisional para la semana siguiente

--- freedom.py
+++ freedom.py
@@ -5,14 +5,18 @@ Created on Tue Mar 16 15:20:21 2021
 @author: rrobles
 """
 # %% Consts
-INDEX = 'IBEX 35'
+
 NUM_DAYS = 365*6
 WEEKS_PER_YEAR = 52
 
-IBEX = ['ANA', 'ACX', 'ACS', 'AENA', 'ALM', 'AMA', 'MTS', 'SABE', 'BKT', 'BBVA', 
+INDEX_SELECTION = 'IBEX 35'
+
+STOCK_SELECTION = ['ANA', 'ACX', 'ACS', 'AENA', 'ALM', 'AMA', 'MTS', 'SABE', 'BKT', 'BBVA', 
         'CABK', 'CLNX', 'CIEA', 'COL', 'ENAG', 'ELE', 'FER', 'FLUI', 'GRLS', 
         'ICAG', 'IBE', 'ITX', 'IDR', 'MAP', 'MEL', 'MRL', 'NTGY', 'PHMR', 'REE',
         'REP', 'SAN', 'SGREN', 'SLRS', 'TEF', 'VIS']
+
+COUNTRY_SELECTION = 'spain'
 
 # %% Imports
 from datetime import datetime, timedelta, date
@@ -38,7 +42,7 @@ def isweekend():
     return datetime.today().weekday() >= 5
     
 # %% Get stocks and indexes
-df_stocks = investpy.stocks.get_stocks(country='spain')
+df_stocks = investpy.stocks.get_stocks(country=COUNTRY_SELECTION)
 df_indexes = investpy.indices.get_index_countries()
 
 # %% Fecth data
@@ -46,17 +50,18 @@ today = datetime.today()
 yearAgo = today - timedelta(days = NUM_DAYS)
 initDate = yearAgo.strftime('%d/%m/%Y')
 
-endDate = findsunday(date.today() - timedelta(days = 6)).strftime('%d/%m/%Y')
+endDate = findsunday(date.today()).strftime('%d/%m/%Y')
+#endDate = findsunday(date.today() - timedelta(days = 6)).strftime('%d/%m/%Y')
 #endDate = date(2021, 4, 4).strftime('%d/%m/%Y')
 #endDate = today.strftime('%d/%m/%Y')
 
 df_list = list()
 
-for row in IBEX: #df_stocks.symbol:
+for row in STOCK_SELECTION: #df_stocks.symbol:
     try:
-        df = investpy.get_stock_historical_data(stock=row, country='spain', from_date=initDate, to_date=endDate, interval='Weekly')
+        df = investpy.get_stock_historical_data(stock=row, country=COUNTRY_SELECTION, from_date=initDate, to_date=endDate, interval='Weekly')
 
-        df_daily = investpy.get_stock_recent_data(stock=row, country='spain', interval='Daily')
+        df_daily = investpy.get_stock_recent_data(stock=row, country=COUNTRY_SELECTION, interval='Daily')
 
         df['Ticker']=row
         if df.shape[0] > WEEKS_PER_YEAR:
@@ -67,7 +72,7 @@ for row in IBEX: #df_stocks.symbol:
         print('Stock unavailable: ', row)
     
     
-df_index = investpy.get_index_historical_data(index=INDEX, country='spain', from_date=initDate, to_date=endDate, interval='Weekly')
+df_index = investpy.get_index_historical_data(index=INDEX_SELECTION, country=COUNTRY_SELECTION, from_date=initDate, to_date=endDate, interval='Weekly')
 df_index['ROC'] = df_index.Close.pct_change() * 100
 
 # %% Calculate Relative Performace y 52 weeks SMA Relative Performance
@@ -90,9 +95,10 @@ df_results_list = list()
 df_equity = pd.DataFrame(index=df.index, columns = ['Equity', 'EquityIndex'])
 
 
-start_date = findsunday(date(2020, 1, 1))
+start_date = findsunday(date(2021, 1, 1))
 #end_date = findsunday(date(2021, 4, 4))
-end_date = findsunday(date.today()) - timedelta(days = 6)
+#end_date = findsunday(date.today()) - timedelta(days = 6)
+end_date = findsunday(date.today())
 
 cumROC = 100
 cumROCIndex = 100
@@ -134,15 +140,19 @@ for dt in daterange(start_date, end_date):
     #Almacenamos equity de sistema y de índice
     df_equity.loc[dt] = cumROC, cumROCIndex 
     
-print('\r\n RESULTADOS ÚLTIMA SEMANA: ', df_results_list[-2].Date[0] + timedelta(days = 8), ' al ',df_results_list[-2].Date[0] + timedelta(days = 14))
-print('\r Cartera: ', df_results_list[-2].Ticker[0], df_results_list[-2].Ticker[1], df_results_list[-2].Ticker[2])
-print('\r Retorno cartera: ', df_results_list[-2].ROCmean[0])
-print('\r Retorno indice: ', df_results_list[-2].ROCIndex[0])
+print('\r\n RESULTADOS ÚLTIMA SEMANA: ', df_results_list[-3].Date[0] + timedelta(days = 8), ' al ',df_results_list[-3].Date[0] + timedelta(days = 14))
+print('\r Cartera: ', df_results_list[-3].Ticker[0], df_results_list[-3].Ticker[1], df_results_list[-3].Ticker[2])
+print('\r Retorno cartera: ', df_results_list[-3].ROCmean[0])
+print('\r Retorno indice: ', df_results_list[-3].ROCIndex[0])
 #print('\r', df_results_list[-2])
 #print('\r', df_results_list[-1])
-print('\r\n PRONÓSTICO SEMANA ACTUAL: ', df_results_list[-1].Date[0] + timedelta(days = 8), ' al ', df_results_list[-1].Date[0] + timedelta(days = 14))
-print('\r Cartera: ', df_results_list[-1].Ticker[0], df_results_list[-1].Ticker[1], df_results_list[-1].Ticker[2])
+print('\r\n RESULTADOS SEMANA ACTUAL: ', df_results_list[-2].Date[0] + timedelta(days = 8), ' al ', df_results_list[-2].Date[0] + timedelta(days = 14))
+print('\r Cartera: ', df_results_list[-2].Ticker[0], df_results_list[-2].Ticker[1], df_results_list[-2].Ticker[2])
+print('\r Retorno latente cartera: ', df_results_list[-2].ROCmean[0])
+print('\r Retorno latente indice: ', df_results_list[-2].ROCIndex[0])
 
+print('\r\n COMPOSICIÓN PROVISIONAL SIGUIENTE CARTERA: ', df_results_list[-1].Date[0] + timedelta(days = 8), ' al ', df_results_list[-1].Date[0] + timedelta(days = 14))
+print('\r Cartera: ', df_results_list[-1].Ticker[0], df_results_list[-1].Ticker[1], df_results_list[-1].Ticker[2])
 
 # %% Plot equity (system + index)
 plt.plot(df_equity.Equity)
